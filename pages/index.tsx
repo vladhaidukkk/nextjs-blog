6b0f7fa3604c35efb8130ddaa8561f0af4f47983
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-02-15 12:31:37 +0200

    refactor: rewrite app on typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
-import Date from '../components/date';
-import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) {
   return (
     <Layout home>
       <Head>
@@ -16,7 +25,7 @@ export default function Home({ allPostsData }) {
           Hello, I'm <b>Vlad</b>. I'm a software engineer.
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -38,11 +47,11 @@ export default function Home({ allPostsData }) {
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
